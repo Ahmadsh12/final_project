bc1672ff51e76e6280a330ab9b6d69d311f0d04b
ahmad sholy <57842204+Ahmadsh12@users.noreply.github.com>
2021-02-14 05:43:54 +0200

    database update

--- database/ecs (1).sql
+++ database/ecs (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 05, 2021 at 04:42 PM
+-- Generation Time: Feb 14, 2021 at 04:42 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.2.34
 
@@ -44,7 +44,8 @@ CREATE TABLE `accepted_vendors` (
 
 INSERT INTO `accepted_vendors` (`av_id`, `av_name`, `av_email`, `av_pass`, `av_mobile`, `av_comname`, `av_image`, `av_desc`) VALUES
 (37, 'Ahmad sholy', 'ahmad@gmail.com', 'A321', 795762203, 'Apple', 'download.png', 'Apple Inc is an American multinational technology company headquartered in Cupertino California that designs develops and sells consumer electronics computer software and online services'),
-(38, 'Yazeed', 'yazeed@gmail.com', 'A123', 795762138, 'Amazon', 'download (1).png', 'Amazon is an American multinational technology company based in Seattle  Washington  which focuses on e commerce cloud computing digital streaming and artificial intelligence');
+(38, 'Yazeed', 'yazeed@gmail.com', 'A123', 795762138, 'Amazon', 'download (1).png', 'Amazon is an American multinational technology company based in Seattle  Washington  which focuses on e commerce cloud computing digital streaming and artificial intelligence'),
+(41, 'Muhanad', 'Muhanad@gmail.com', 'A123', 795762138, 'zara', 'z.jpg', 'Zara SA stylized as ZARA is a Spanish apparel retailer based in Arteixo in Galicia Spain The company specializes in fast fashion and products include clothing accessories shoes swimwear beauty and perfumes It is the largest company in the Inditex group the world s largest apparel retailer');
 
 -- --------------------------------------------------------
 
@@ -122,6 +123,13 @@ CREATE TABLE `orders` (
   `cust_id` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `order_date`, `cust_id`) VALUES
+(40, '2021-02-10', 11);
+
 -- --------------------------------------------------------
 
 --
@@ -134,6 +142,13 @@ CREATE TABLE `order_details` (
   `product_id` int(3) NOT NULL,
   `product_qty` int(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`order_details_id`, `order_id`, `product_id`, `product_qty`) VALUES
+(36, 40, 99, 1);
 
 -- --------------------------------------------------------
 
@@ -158,7 +173,12 @@ CREATE TABLE `products` (
 
 INSERT INTO `products` (`pro_id`, `pro_name`, `pro_desc`, `pro_price`, `pro_image`, `cat_id`, `av_id`, `pro_qty`) VALUES
 (98, 'iPhone 12 ', 'the best', 829, 'iphone-12-family-select-2020.jpg', 27, 37, 121),
-(99, 'Echo Dot ( 4th Gen )', 'This bundle contains Echo Dot (4th Gen) and Sengled Bluetooth bulb Automate your home set up with Sengled and Alexa  use your voice to control your lights', 60, '61tniQTsePL._AC_SX679_.jpg', 25, 38, 300);
+(99, 'Echo Dot ( 4th Gen )', 'This bundle contains Echo Dot (4th Gen) and Sengled Bluetooth bulb Automate your home set up with Sengled and Alexa  use your voice to control your lights', 60, '61tniQTsePL._AC_SX679_.jpg', 25, 38, 300),
+(109, 'Jacket', 'the new designs their always trend conscious designers are delivering a sophisticated men s attire in many occasions bordering on the formal informal note Colour tones go from the very present white to sand colours with the touch of red and navy of course combined with the inevitable striped tshirt', 300, '99.jpg', 28, 41, 14),
+(111, 'Jacket', 'This zara leather jacket is heavy quality', 399, 'product-jpeg-500x500.png', 28, 41, 12),
+(113, 'Jacket', 'Zara design Men s Jacket Winter new thickening leisure young men s Korean version of handsome leather Free Belt', 199, 'zara-design-men-jacket-500x500.png', 28, 41, 10),
+(115, 'Smart Plug', 'Smart Plug Gosund Smart WiFi Outlet Works with Alexa and Google Home', 25, '617bTlmaCsL._AC_SL1500_ (1).png', 25, 38, 122),
+(116, 'Smart Alexa Light Bulb', 'Smart Alexa Light Bulb 75W Equivalent E26 8W Upgraded Gosund Led WiFi Bulb A19 Dimmable Works with Amazon Echo Google Home', 30, '61YYS0GKUOL._AC_SL1500_.png', 25, 38, 113);
 
 -- --------------------------------------------------------
 
@@ -176,13 +196,6 @@ CREATE TABLE `vendor` (
   `ven_image` text NOT NULL,
   `ven_desc` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `vendor`
---
-
-INSERT INTO `vendor` (`ven_id`, `ven_name`, `ven_email`, `ven_password`, `ven_mobile`, `ven_comname`, `ven_image`, `ven_desc`) VALUES
-(27, 'Muhanad', 'Muhanad@gmail.com', 'A123', 795762138, 'zara', 'z.jpg', 'Zara SA stylized as ZARA is a Spanish apparel retailer based in Arteixo in Galicia Spain The company specializes in fast fashion and products include clothing accessories shoes swimwear beauty and perfumes It is the largest company in the Inditex group the world s largest apparel retailer');
 
 --
 -- Indexes for dumped tables
@@ -247,7 +260,7 @@ ALTER TABLE `vendor`
 -- AUTO_INCREMENT for table `accepted_vendors`
 --
 ALTER TABLE `accepted_vendors`
-  MODIFY `av_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `av_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `admin`
@@ -271,19 +284,19 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `order_id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `order_details_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `order_details_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `pro_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `pro_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
 
 --
 -- AUTO_INCREMENT for table `vendor`
